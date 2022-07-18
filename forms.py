d53fba9fc3bda5c52bce457de9e377dc962eb1db
Joshua Lee <joshualee@Joshuas-MacBook-Air.local>
2022-07-17 20:57:14 -0400

    add bootstrap and responsiveness

--- forms.py
+++ forms.py
@@ -5,12 +5,13 @@ from wtforms.validators import DataRequired
 
 
 class geteqn(FlaskForm):
-    reactantsStr = StringField(label='Reactants', 
-        validators=[DataRequired()])
-    productsStr = StringField(label='Products', 
-        validators=[DataRequired()])
+    reactantsStr = StringField(label='Reactants',
+                               validators=[DataRequired()])
+    productsStr = StringField(label='Products',
+                              validators=[DataRequired()])
 
-    equationCheck = SubmitField(label='submit')
+    equationCheck = SubmitField(label='Submit')
+
 
 class backToMainPg(FlaskForm):
-    backToMain = SubmitField(label = 'Check a different equation')
+    backToMain = SubmitField(label='Check a different equation')
